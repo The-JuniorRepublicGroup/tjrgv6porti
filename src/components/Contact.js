1ad8ce7a24e513d3e9d68dd4757c1a3248412cb2
freeCCaccount <rokafor89@gmail.com>
2017-07-07 16:16:28 -0400

    added some stuff for portfolio component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,20 +2,66 @@ import React, {Component} from 'react';
 
 
 export default class Contact extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name : '',
+            email : 'testemail@email.com',
+            message : 'this is a test message'
+        }
+    }
+
+  
+
+    sendEmail(event) {
+        // do something with email here
+        event.preventDefault()
+
+        fetch('/contactus', {
+            method: 'POST',
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+            email: this.state.email,
+            name: this.state.name,
+            message: this.state.message
+            // then continue this with the other inputs, such as email body, etc.
+            })
+        })
+        .then((response) => response.json())
+        .then((responseJson) => {
+            if (responseJson.success) {
+            this.setState({formSent: true})
+            }
+            else this.setState({formSent: false})
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    }
     
     render(){
+
+        
+
         return(
                 <div className="container">
                     <h2>Contact Us</h2>
 
                     <div>
+
+                        <p>{this.state.name}</p>
+
                         <form>
                             <input type="text" name="name" value={this.state.name} placeholder="Enter Name here..." />
                             <input type="email" name="email" value={this.state.email} placeholder="Enter Email here..." />
                             <br />
                             <input type="textarea" name="message" value={this.state.message} placeholder="Enter Message here..." />
                             <br />
-                            <button type="submit">SEND</button>
+                            <button type="submit" onClick={this.sendEmail.bind(this)} >SEND</button>
                         </form>
                     </div>
                 </div>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,7 +5,18 @@ export default class Portfolio extends Component{
     
     render(){
         return(
-                <h1>This is the Portfolio Component</h1>
+
+            <div className="portfolio">
+                <h2>This is the Portfolio Component</h2>
+                <div className="project">
+                    <img src="https://s17.postimg.org/xixeyesbz/image_grid.png" alt="#" />
+                </div>
+
+                <div className="info">
+                    <h3>Project Name</h3>
+                    <p>Something Describing This Project</p>
+                </div>
+            </div>
             )
     }
     
